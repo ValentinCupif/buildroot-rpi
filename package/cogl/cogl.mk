8cac49902cf31c666271357114c82b4465f848db
rbp <m.sieben@metrological.com>
2013-06-13 11:35:39 +0200

    buildroot : added cogl

---
+++ package/cogl/cogl.mk
@@ -0,0 +1,38 @@
+#############################################################
+#
+# cogl
+#
+#############################################################
+
+COGL_VERSION = 1.12.2
+COGL_SITE = http://ftp.gnome.org/pub/GNOME/sources/cogl/1.12/
+COGL_SOURCE=cogl-$(COGL_VERSION).tar.xz
+
+COGL_DEPENDENCIES = pango
+
+COGL_CONF_OPT =  \
+	--enable-gles1=no \
+	--enable-gles2=yes \
+	--enable-glx=no \
+	--enable-gl=no \
+	--enable-wgl=no \
+	--enable-gdl-egl-platform=yes \
+	--enable-xlib-egl-platform=no \
+	--enable-gtk-doc-html=no \
+	--enable-cairo=no \
+	--enable-cogl-pango=no \
+	--enable-gdl-egl-platform=no
+
+define COGL_CONFIGURE_CMDS
+endef
+
+define COGL_BUILD_CMDS
+endef
+
+define COGL_INSTALL_TARGET_CMDS
+endef
+
+define COGL_UNINSTALL_TARGET_CMDS
+endef
+
+$(eval $(autotools-package))
