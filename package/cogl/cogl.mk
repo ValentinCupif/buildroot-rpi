f2b4926d9df386323a72f342afd1e8de2314c7ce
rbp <m.sieben@metrological.com>
2013-06-17 19:33:00 +0200

    cogl : update makefile

--- package/cogl/cogl.mk
+++ package/cogl/cogl.mk
@@ -16,26 +16,38 @@ COGL_CONF_OPT =  \
 	--enable-gles1=no \
 	--enable-gles2=yes \
 	--enable-glx=no \
-	--enable-gl=no \
 	--enable-wgl=no \
 	--enable-xlib-egl-platform=no \
 	--enable-gtk-doc-html=no \
 	--enable-cairo=no \
-	--enable-cogl-pango=no \
-	--enable-gdl-egl-platform=no
+	--enable-gl=no \ 
+	--enable-gdl-egl-platform=no \
+	--enable-wayland-egl-platform=no \
+	--enable-wayland-egl-server=no 
+
+ifeq ($(BR2_PACKAGE_PANGO), y)
+COGL_DEPENDENCIES += pango
+COGL_CONF_OPT += \
+	--enable-cogl-pango=yes
+else
+COGL_CONF_OPT += \
+	--enable-cogl-pango=no 
+endif
 
 define COGL_POST_INSTALL_FILES
 	$(INSTALL) -m 755 -D $(@D)/cogl/*.pc $(STAGING_DIR)/usr/lib/pkgconfig/                                                       
-
 	$(INSTALL) -m 755 -D $(@D)/cogl-pango/*.pc $(STAGING_DIR)/usr/lib/pkgconfig/                                                       
-	$(INSTALL) -m 755 -D $(@D)/cogl-gles2/*.pc $(STAGING_DIR)/usr/lib/pkgconfig/                                                       
-
 	mkdir -p $(STAGING_DIR)/usr/include/cogl/ 
 	$(INSTALL) -m 755 -D $(@D)/cogl/*.h $(STAGING_DIR)/usr/include/cogl/ 
 
 	mkdir -p $(STAGING_DIR)/usr/include/cogl-pango/ 
 	$(INSTALL) -m 755 -D $(@D)/cogl-pango/*.h $(STAGING_DIR)/usr/include/cogl-pango/ 
-endef  
+
+	$(INSTALL) -m 755 -D $(@D)/cogl/.libs/*.so* $(STAGING_DIR)/usr/lib/ 
+	$(INSTALL) -m 755 -D $(@D)/cogl-gles2/.libs/*.so* $(STAGING_DIR)/usr/lib/ 
+	$(INSTALL) -m 755 -D $(@D)/cogl-pango/.libs/*.so* $(STAGING_DIR)/usr/lib/ 
+endef
+
 
 COGL_POST_INSTALL_TARGET_HOOKS += COGL_POST_INSTALL_FILES
 
