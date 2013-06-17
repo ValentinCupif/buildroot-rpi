9e9855ab606e9d16e8dcc42a9b7a75f8ad09b0d7
rbp <m.sieben@metrological.com>
2013-06-17 22:05:22 +0200

    clutter : update makefile

--- package/clutter/clutter.mk
+++ package/clutter/clutter.mk
@@ -8,7 +8,7 @@ CLUTTER_VERSION = 1.14.4
 CLUTTER_SITE = http://ftp.gnome.org/pub/GNOME/sources/clutter/1.14/
 CLUTTER_SOURCE=clutter-$(CLUTTER_VERSION).tar.xz
 
-CLUTTER_DEPENDENCIES = cogl json-glib atk
+CLUTTER_DEPENDENCIES = cogl json-glib atk wayland gdk-pixbuf libxkbcommon gdk-pixbuf
 
 CLUTTER_AUTORECONF = yes
 
@@ -17,13 +17,17 @@ CLUTTER_CONF_OPT =  \
 	--enable-win32-backend=no \
 	--enable-quartz-backend=no \
 	--enable-gdk-backend=no \
-	--enable-wayland-backend=no \
+	--enable-wayland-backend=yes \
 	--enable-cex100-backend=no \
-	--enable-wayland-compositor=no \
+	--enable-wayland-compositor=yes \
 	--enable-tslib-input=no \
 	--enable-evdev-input=no \
 	--enable-xinput=no \
 	--enable-gdk-pixbuf=no \
-	--enable-egl-backend=yes
+	--enable-egl-backend=no \
+	--includedir=$(STAGING_DIR)/gdk-pixbuf-2.0/ \
+	--disable-examples \
+	--disable-tests
+
 
 $(eval $(autotools-package))
