5efa34db6565a779a1835b7d6d2d9fd7b7a892c4
rbp <m.sieben@metrological.com>
2013-06-20 16:57:07 +0200

    webkit-clutter : update makefile

--- package/webkit-clutter/webkit-clutter.mk
+++ package/webkit-clutter/webkit-clutter.mk
@@ -8,22 +8,32 @@
 WEBKIT_CLUTTER_VERSION = 6960cd716129f050c62d0d16fbe92f686fa225e8
 WEBKIT_CLUTTER_SITE_METHOD = git
 WEBKIT_CLUTTER_SITE = git://git.collabora.co.uk/git/webkit-clutter.git
-#http://cgit.collabora.com/git/webkit-clutter.git
+#WEBKIT_CLUTTER_SITE = http://cgit.collabora.com/git/webkit-clutter.git
 
-WEBKIT_CLUTTER_DEPENDENCIES = clutter
+WEBKIT_CLUTTER_DEPENDENCIES = clutter gperf webp libsoup libxslt
+#libsecret
 
-WEBKIT_CLUTTER_AUTORECONF = yes
+WEBKIT_CLUTTER_CONF_OPT += \
+	--enable-credential-storage=no \
+	--enable-geolocation=no \
+	--with-target=directfb \
+	 --enable-video=no
 
-define WEBKIT_CLUTTER_CONFIGURE_CMDS
+define WEBKIT_CLUTTER_RUN_AUTOCONF
+
+	(cd $(@D); \
+		PATH=$(PATH):$(HOST_DIR)/bin:$(HOST_DIR)/usr/bin \
+		LIBTOOLIZE=$(LIBTOOLIZE) \
+		ACLOCAL_FLAGS=$(ACLOCAL_FLAGS) \
+		ACLOCAL="$(ACLOCAL)" \
+		AUTOHEADER=$(AUTOHEADER) \
+		AUTOCONF=$(AUTOCONF) \
+		AUTOMAKE=$(AUTOMAKE) \
+		AUTOM4TE=$(HOST_DIR)/usr/bin/autom4te \
+                NOCONFIGURE=1 \
+		./autogen.sh)
 endef
 
-define WEBKIT_CLUTTER_BUILD_CMDS
-endef
-
-define WEBKIT_CLUTTER_INSTALL_TARGET_CMDS
-endef
-
-define WEBKIT_CLUTTER_UNINSTALL_TARGET_CMDS
-endef
+WEBKIT_CLUTTER_PRE_CONFIGURE_HOOKS += WEBKIT_CLUTTER_RUN_AUTOCONF
 
 $(eval $(autotools-package))
